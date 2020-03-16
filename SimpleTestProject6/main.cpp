9d8424b471e91f84fa70512c9265cec2bab1f15b
Kem White <kemwhite@outlook.com>
2020-03-15 21:35:36 -0400

    update

--- SimpleTestProject6/main.cpp
+++ SimpleTestProject6/main.cpp
@@ -1,18 +1,25 @@
-#include <refExample.h>
-#include <simple.h>
+
 #include <arrayTest.h>
 #include <polarRectTest.h>
-#include <vectorTest.h>
+#include <refExample.h>
+#include <simple.h>
 #include <testStock00.h>
+#include <vectorTest.h>
+#include <cstdlib>
+#include <iostream>
 
-int main()
-{
-	refExample();     //do some work on pointers and references
-	simple();         //call a ridiculously simple function
-	arrayTest();      //call a function that works on an array and a for loop
-	polarRectTest();  //call to do some simple Cartesian/polar conversions
-	vectorTest();     //duplicate the array test function using vectors
-	testStock00();    //test of the Stock class
-	
-	return 0;
+int main() {
+    refExample();     // do some work on pointers and references
+    simple();         // call a ridiculously simple function
+    arrayTest();      // call a function that works on an array and a for loop
+    polarRectTest();  // call to do some simple Cartesian/polar conversions
+    vectorTest();     // duplicate the array test function using vectors
+    testStock00();    // test of the Stock class
+
+    std::cout << "This is a simple message." << std::endl;
+    double pi = 3.14159265359;
+    double twoPi = 2.0 * pi;
+    std::cout << " pi= " << pi << std::endl;
+    std::cout << " 2pi= " << twoPi << std::endl;
+    return EXIT_SUCCESS;
 }
